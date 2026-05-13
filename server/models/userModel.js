5556aadafead3b142823f2a8c6e8d4707a8cb3f5
Luong <ctpluong@gmail.com>
2026-05-13 16:52:16 +0700

    complete app

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -43,7 +43,7 @@ const userSchema = mongoose.Schema(
 );
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await (enteredPassword== this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 userSchema.pre('save', async function (next) {
